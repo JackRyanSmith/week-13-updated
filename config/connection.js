27c6b99abda7599f2535d850fd767f3987e5d888
JackRyan_Smith <JackRyan3d@gmail.com>
2020-10-28 18:06:48 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
+let connection = mysql.createConnection(process.env.JAWSDB_URL || {
   host: "localhost",
   port: 3306,
   user: "root",
